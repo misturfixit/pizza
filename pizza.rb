e3e04e5fc9442ae6167684563158f9d1e9d10a50
misturfixit <japullins420@gmail.com>
2017-06-29 13:47:23 -0400

    ooof

--- pizza.rb
+++ pizza.rb
@@ -1,6 +1,4 @@
-def size 
-	size = ["Small", "Med", "Large"].sample
-end
+
 
 def crust
 	crust = ["Thin", "Brick", "Pan"].sample
@@ -11,7 +9,7 @@ def meats
 end
 
 def cheese 
-	cheese = ["Mozz", "Gouda", "Goat", "Sharp Goat", "Ferret feta"].sample
+	cheese = ["Mozz", "Gouda", "Goat", "Sharp Goat", "Ferreteta"].sample
 end
 
 def othertopp 
@@ -20,47 +18,49 @@ end
 
 print "Welcome to HaHa's Pizza....We don't care what you want....Except.."
 #puts #spaceee
+
 puts "How many pizzzas You want Bruh ?" 
 	x = gets.chomp.to_i
-    count =  1
+    count =     1
     cost =  0
 	x.times do
-    size = size
+    #size = size
 puts "Pizza #{count}"
-    count +=  1
-
-  end
+    count += 1
 
 
+def size 
+size = ["Small", "Med", "Large"].sample
 
+end
 
- puts "size is #{size}"
-   if  size == "Small"
-
-  then cost =  5.00 
+puts "size is #{size}"
+   if  size == "Small"    
+   then cost =  5.00 
 
 puts  "Price is #{cost}"
-  elsif size == "Med"
-   
+  elsif size == "Med"   
   then cost = 12.50  
 
 puts  "Price is #{cost}"
-  else   size == "Large"
-  
-   cost = 15.00 
+  else   size == "Large"  
+         cost = 15.00 
+
+
 puts "Price is #{cost}"
-
-
+ 
 end
+#loop do 
 
 
 
 puts size
+#puts cost
 puts crust
 puts meats
 puts cheese
 puts othertopp
 
-
+end
 
 
