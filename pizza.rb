9035afbfd3bd56f10e60e62e489e621821a389f2
misturfixit <japullins420@gmail.com>
2017-07-05 09:37:24 -0400

    tipha

--- pizza.rb
+++ pizza.rb
@@ -27,9 +27,13 @@ def extra
 end
 
 def delivery
-	delivery = [" yohouse", "notyohouse"].sample
+	delivery = ["yohouse", "padizzahouse"].sample
 end
 
+def tip
+  tip = ["quicknuff", "notnuff"].sample
+end
+  
 
 #p " hullo there".titlize 
 
@@ -38,18 +42,23 @@ p "How many pizzzas You want Bruh ?"
 count = 1
 cost = 0
 x.times do
+  drvr_tip = tip 
   piz_size = size
   mor_stuf = extra#.rand
-  
+  you_gitnit = delivery
   p "Pizza #{count}"
      count +=1
   puts piz_size                                                            
   puts crust
-  puts mor_stuf                  
+  puts you_gitnit
+  puts mor_stuf
+  #puts you_gitnit                  
   puts sauce                
   puts meats    #if  "extrameat"  then cost += 2                
   puts cheese   #if "extracheese" then cost += 2                         
   puts othertopp                           
+  puts drvr_tip
+
     if piz_size == "Small"    
       cost += 5
       puts "price is $#{cost}"
@@ -65,20 +74,36 @@ x.times do
       cost += 2
       puts  "Price w/extra is $#{cost}"
     elsif  mor_stuf == "noextrameat"
-    
       puts "No extra"	
     elsif mor_stuf == "extracheese"
       cost += 2
- 	  puts " Price w/extra is $#{cost}"
+ 	   puts " Price w/extra is $#{cost}"
     elsif  mor_stuf == "noextracheese"
-    
       puts " No extra"	
-     	
-     
     end	
-     	 
+
+    if drvr_tip == "quicknuff"
+      cost += 5
+      puts "thanks brah $#{cost}"
+     elsif drvr_tip == "notnuff"
+      puts "notip"
+    end         
+
+    #case you_gitnit
+      #puts "price w/delivery is $#{cost}"
+    if you_gitnit == "yohouse"
+      cost += 175
+      puts "price w/delivery is $#{cost}" 
+    elsif you_gitnit == "padizzahouse"
+      puts "Okay see you in 30 min "	
+    end
+
+
      
+  puts 
+  puts
   puts
 end
+puts "we deliver by helicopter"
 puts "Total cost is $#{'%.2f' %(cost * 1.06)}"
    
